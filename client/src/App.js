72593b53eebbb9db69f04de6891232fdcc14d69a
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-03 15:46:32 +0100

    created a Form where the user can input a todo

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,15 @@ import React, { Fragment } from 'react'
 
 import './App.css';
 
+import InputTodo from "./components/InputTodo"
+
 export default function App() {
   return (
     <Fragment>
-      
+      <div className="container">
+
+         <InputTodo />
+      </div>
     </Fragment>
   );
 }
