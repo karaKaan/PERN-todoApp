27744bcf1d196e8b7fedd455b78767f50d022a54
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-03 10:38:56 +0100

    A todo can now be created + it will be saved in the db

--- server/index.js
+++ server/index.js
@@ -7,7 +7,31 @@ const pool = require('./db');
 app.use(cors())
 app.use(express.json())
 
+
 //Routes
+
+//create a todo
+
+app.post("/todos", async(req, res) => {
+    try {
+        const {description} = req.body;
+        const newTodo = await pool.query("insert into todo (description) values($1) returning *",[description])
+        res.json(newTodo.rows[0])
+
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
+
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 })
