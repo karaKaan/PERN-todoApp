92076a780deaea20ea9c855cd2689e00a7ff01e2
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-03 10:28:19 +0100

    connected the postgres to the server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const pool = require('./db');
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-
+//Routes
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 })
